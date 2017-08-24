3fd0d3656d557873795d6e9e285ee81fdb86e5a7
Abel Onditi <abel.onditi@gmail.com>
2017-08-24 12:40:17 +0300

    GCodeParser init
    Added GCodeParser class

---
+++ src/GCodeParser.h
@@ -0,0 +1,17 @@
+/*
+ * GCodeParser.h
+ *
+ *  Created on: 24 Aug 2017
+ *      Author: Eibl-PC
+ */
+
+#ifndef GCODEPARSER_H_
+#define GCODEPARSER_H_
+
+class GCodeParser {
+public:
+	GCodeParser();
+	virtual ~GCodeParser();
+};
+
+#endif /* GCODEPARSER_H_ */
