b796c085af4db7e2fa649af90f2b19af41c146e0
Aki <mushut@gmail.com>
2017-08-25 08:49:51 +0300

    Some minor changes
    Included some things that were forgot to include at first.

--- src/ESPBeam.cpp
+++ src/ESPBeam.cpp
@@ -28,12 +28,26 @@
 static void vReadWriteUART(void *pvParameters) {
 
 	while(1) {
+		char character = 255;
+
 		// While loop for reading characters from serial
 		while ((character = Board_UARTGetChar()) != 255) {
 					Board_UARTPutChar(character);
 		}
 	}
 }
+
+/* the following is required if runtime statistics are to be collected */
+extern "C" {
+
+void vConfigureTimerForRunTimeStats( void ) {
+	Chip_SCT_Init(LPC_SCTSMALL1);
+	LPC_SCTSMALL1->CONFIG = SCT_CONFIG_32BIT_COUNTER;
+	LPC_SCTSMALL1->CTRL_U = SCT_CTRL_PRE_L(255) | SCT_CTRL_CLRCTR_L; // set prescaler to 256 (255 + 1), and start timer
+}
+
+}
+/* end runtime statictics collection */
 
 int main(void) {
 
